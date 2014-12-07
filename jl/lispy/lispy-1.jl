267297a89ce04d850e66bffba26d56ac4d61a5eb
Isaiah Norton <isaiah.norton@gmail.com>
2014-12-07 09:22:03 -0500

    Add initial environment dict

--- jl/lispy/lispy-1.jl
+++ jl/lispy/lispy-1.jl
@@ -31,6 +31,36 @@ function atom(token)
     end
 end
 
+Env = Dict{String, Function}
+function standard_env()
+    env = Env()
+    merge!(env, Dict(
+        "abs"   => abs,
+        "append"=> push!,
+        "apply" => apply,
+        "begin" => x -> x[end],
+        "car"   => x -> x[1],
+        "cdr"   => x -> x[2:end],
+        "cons"  => (x,y) -> push!(Any[x], y),
+        "eq?"   => isa,
+        "equal?"=> isequal,
+        "length"=> length,
+        "list"  => x -> [x],
+        "list?" => x -> isa(x, Vector),
+        "map"   => map,
+        "max"   => max,
+        "min"   => min,
+        "not"   => !,
+        "null?" => isempty,
+        "number?"=> x -> isa(x, Number),
+        "procedure?" => x -> isa(x, Base.Callable),
+        "round" => round,
+        "symbol?"=> x -> isa(x, Symbol))
+    )
+end
+
+global_env = standard_env()
+
 # Test cases
 program = "(begin (define r 10) (* pi (* r r)))" 
 @test tokenize(program) == 
